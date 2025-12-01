77a6dddefd9bf05ff661f44dc709c7b6c9c30995
Trevor Bernard <tbernard@aypa.com>
2025-12-01 11:24:27 -0500

    chore: upgrade nix-darwin to 25.11

--- aypa/etc/nix-darwin/flake.nix
+++ aypa/etc/nix-darwin/flake.nix
@@ -1,9 +1,7 @@
 {
-  description = "Example nix-darwin system flake";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     termcopy.url = "github:trevorbernard/termcopy";
     claude-code.url = "path:../../../nix-common/claude-code";
