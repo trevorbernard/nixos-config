a2a88333214d2b2f961183ef5f78ec242b8df44c
Trevor Bernard <tbernard@aypa.com>
2025-12-08 14:50:26 -0500

    chore: upgrade claude-code to 2.0.61

--- nix-common/claude-code/default.nix
+++ nix-common/claude-code/default.nix
@@ -6,14 +6,14 @@
 
 buildNpmPackage rec {
   pname = "claude-code";
-  version = "2.0.57";
+  version = "2.0.61";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-mnSM1FFLrVQwmS3MgRoLg3lIDPJ7ERiWHLDsQoxRIxA=";
+    hash = "sha256-701TwdPtqudwKuphVngMiI6TbM9y1S/HFVTREhkHgdY=";
   };
 
-  npmDepsHash = "sha256-AUvWdO8JMcB8ORKyz5X94UQBihdNVdt5tzlTg2D3aX8=";
+  npmDepsHash = "sha256-c7rjWsbd5GMqCmkrh5sUlHo9KEARRGOTT2TdHk1aRkw=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
