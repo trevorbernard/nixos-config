57e6a38e907ac667e319e92030f82f650bd80fd1
Trevor Bernard <trevor.bernard@gmail.com>
2026-01-20 11:34:04 -0500

    chore: upgrade claude-code to 2.1.12

--- knowhere/nixos/nix/claude-code/default.nix
+++ knowhere/nixos/nix/claude-code/default.nix
@@ -6,14 +6,14 @@
 
 buildNpmPackage rec {
   pname = "claude-code";
-  version = "2.0.76";
+  version = "2.1.12";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-46IqiGJZrZM4vVcanZj/vY4uxFH3/4LxNA+Qb6iIHDk=";
+    hash = "sha256-JX72YEM2fXY7qKVkuk+UFeef0OhBffljpFBjIECHMXw=";
   };
 
-  npmDepsHash = "sha256-xSNyYImDpsW6AltA7d0ayMsfVaBcnyPIQOg/Ea2cGNk=";
+  npmDepsHash = "sha256-iJwtwAYb/+1Une6Tjxek5ccf4ui3tYWy4kNlHES9He4=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
