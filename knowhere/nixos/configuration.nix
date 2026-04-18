ae182aef1e6e462d6b3e13493e728af3cf113cf2
Trevor Bernard <trevor.bernard@gmail.com>
2026-04-18 10:00:50 -0400

    fix(knowhere): remove programs.mosh to prevent global firewall override

--- knowhere/nixos/configuration.nix
+++ knowhere/nixos/configuration.nix
@@ -272,8 +272,6 @@
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
 
-  programs.mosh.enable = true;
-
   services.syncthing = {
     enable = true;
     user = "tbernard";
